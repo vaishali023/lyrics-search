680e55aceccf80f79e8fbaa1326bfec28acb8ae6
vaishali023 <vaishalichaudhary011@gmail.com>
2022-05-16 12:20:03 -0400

    Get and display song lyrics

--- script.js
+++ script.js
@@ -53,6 +53,19 @@ async function getMoreSongs(url) {
     showData(data);
   }
 
+  // Get lyrics for song
+async function getLyrics(artist, songTitle) {
+    const res = await fetch(`${apiURL}/v1/${artist}/${songTitle}`);
+    const data = await res.json();
+  
+    const lyrics = data.lyrics.replace(/(\r\n|\r|\n)/g, '<br>');
+  
+    result.innerHTML = `<h2><strong>${artist}</strong> - ${songTitle}</h2>
+    <span>${lyrics}</span>`;
+  
+    more.innerHTML = '';
+  }
+
 // Event listeners
 form.addEventListener('submit', e => {
     e.preventDefault();
@@ -65,3 +78,17 @@ form.addEventListener('submit', e => {
       searchSongs(searchTerm);
     }
   });
+
+  
+// Get lyrics button click
+result.addEventListener('click', e => {
+    const clickedEl = e.target;
+  
+    if (clickedEl.tagName === 'BUTTON') {
+      const artist = clickedEl.getAttribute('data-artist');
+      const songTitle = clickedEl.getAttribute('data-songtitle');
+  
+      getLyrics(artist, songTitle);
+    }
+  });
+  
